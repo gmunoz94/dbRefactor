9d3204e4d47d14dd410f988011ddc20d486de1a0
Gerardo Munoz <g.munozii@icloud.com>
2021-09-28 20:45:34 -0500

    finished typedefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -40,7 +40,19 @@ const resolvers = {
             );
 
             return book;
-        }
+        },
+
+        deleteBook: async ({ user, params }, res) => {
+            const updatedUser = await User.findOneAndUpdate(
+              { _id: user._id },
+              { $pull: { savedBooks: { bookId: params.bookId } } },
+              { new: true }
+            );
+            if (!updatedUser) {
+              return res.status(404).json({ message: "Couldn't find user with this id!" });
+            }
+            return res.json(updatedUser);
+          },
     }
 }
 
